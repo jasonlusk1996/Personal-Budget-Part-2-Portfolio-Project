0ff686443ff267ba2ad7cff8d112c9cedac019ae
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-18 12:39:00 -0500

    Add envelopes and related data

---
+++ envelopes.js
@@ -0,0 +1,30 @@
+const envelopes = [{
+    id: 1,
+    name: "Groceries",
+    budget: 500,
+    spent: 0
+},
+{
+    id: 2,
+    name: "Rent",
+    budget: 1200,
+    spent: 0
+},
+{
+    id: 3,
+    name: "Entertainment",
+    budget: 300,
+    spent: 0
+}, {
+    id: 4,
+    name: "Utilities",
+    budget: 200,
+    spent: 0
+}, {
+    id: 5,
+    name: "Transportation",
+    budget: 150,
+    spent: 0
+}];
+
+module.exports = envelopes;
