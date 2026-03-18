2d872941601a2acbb6ce402cd81e3dc1fd1b7493
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-03-18 12:17:55 -0500

    Add simple hello world message when visiting homepage

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const run = require('./app.js');
 const PORT = 3000;
+app.use('/',run);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
