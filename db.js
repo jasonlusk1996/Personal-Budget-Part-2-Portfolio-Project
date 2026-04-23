e45b458f421986f1aa227899e2256f7d7a727acf
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-23 16:54:22 -0500

    Connect DB to node server

---
+++ db.js
@@ -0,0 +1,25 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, 'db.env') });
+const {Pool}=require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+});
+
+const dbConnect = async ()=>{
+    try{
+        const client = await pool.connect();
+        console.log('Connection successful');
+        client.release();
+    } catch (err){
+        console.log(err);
+    }
+};
+
+dbConnect();
+
+module.exports= dbConnect;
