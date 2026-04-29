56b519b49971a8b1954b316439121c947665b933
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-29 16:34:02 -0500

    Create appRouter to make routes shorter

--- transactionsRoutes.js
+++ transactionsRoutes.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = express();
 const pool = require("./db.js");
 app.use(express.json());
+
+//create a router for envelope-related routes and mount it on the /envelopes path
+appRouter = express.Router();
+app.use('/transactions', appRouter);
