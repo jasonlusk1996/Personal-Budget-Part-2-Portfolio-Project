387703c31e0d151a1924c079571480657d7969fe
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-29 16:47:33 -0500

    Add /get route to get all transactions

--- transactionsRoutes.js
+++ transactionsRoutes.js
@@ -3,4 +3,15 @@ const transactionRoutes = express();
 const pool = require("./db.js");
 transactionRoutes.use(express.json());
 
+//gets all transactions
+transactionRoutes.get('/', async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM transactions");
+        res.status(200).json(result.rows);
+        }catch(err){
+            console.log(err);
+            res.status(500).send('Database connection error');
+        }
+});
+
 module.exports = transactionRoutes;
