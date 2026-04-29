5a4ac41fbaf18a102bcd23f7b3c7c761e6397773
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-29 16:47:08 -0500

    Remove router from trasactionRoutes.js

--- transactionsRoutes.js
+++ transactionsRoutes.js
@@ -3,8 +3,4 @@ const transactionRoutes = express();
 const pool = require("./db.js");
 transactionRoutes.use(express.json());
 
-//create a router for envelope-related routes and mount it on the /envelopes path
-appRouter = express.Router();
-transactionRoutes.use('/transactions', appRouter);
-
 module.exports = transactionRoutes;
