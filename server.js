ec3579c2e27f670326e1597d14e64c882fb81041
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-29 16:39:28 -0500

    Import transactionRoutes.js to server.js and initialize it

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const run = require('./app.js');
+const transactionRoutes = require('./transactionsRoutes.js');
 const PORT = 3000;
 app.use('/',run);
+app.use('/transactions',transactionRoutes);
 // Start the server on the specified port and display a message in the console
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
