060a377c256d8ccf8bc10d884813717cf02ac355
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-04-30 10:24:54 -0500

    Add get route to get specific transaction based on ID

--- transactionsRoutes.js
+++ transactionsRoutes.js
@@ -14,4 +14,19 @@ transactionRoutes.get('/', async (req, res) => {
         }
 });
 
+//gets a specific transaction by id from the PG DB and returns it in the response
+transactionRoutes.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try{
+        const {rows} = await pool.query("SELECT * FROM transactions WHERE id = $1", [id]);
+        if(rows.length===0){
+            res.status(404).send("Transaction not found");
+        }
+        res.status(200).json(rows[0]);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Database connection error");
+    }
+});
+
 module.exports = transactionRoutes;
