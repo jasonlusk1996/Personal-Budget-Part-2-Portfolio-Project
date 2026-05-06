74bfb20a92ba2d33504fca65a13b008ec5147538
jasonlusk1996 <jasonlusk1996@gmail.com>
2026-05-06 11:58:55 -0500

    Create Swagger.js file to require swagger modules

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: { title: 'Personal Budget', version: '1.0.0' },
+    servers: [{ url: 'http://localhost:3000' }],
+  },
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+// Export these so app.js can use them
+module.exports = { swaggerUi, swaggerSpec };
